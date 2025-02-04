54ffde017c8d31d0ca0a2167095bed5db5f814ef
Kartavya Mantri <127770922+kartavyamantri@users.noreply.github.com>
2025-02-04 10:49:10 +0530

    Time: 1 ms (86.92%), Space: 41.4 MB (7.11%) - LeetHub

--- 0069-sqrtx/0069-sqrtx.java
+++ 0069-sqrtx/0069-sqrtx.java
@@ -1,12 +1,21 @@
 class Solution {
     public int mySqrt(int x) {
         if(x == 0) return 0;
-        int ans = 1;
-        for(int i = 1; i <= Math.sqrt(x); i++) {
-            if(i * i <= x) {
-                ans = i;
-            }
+        // int ans = 1;
+        // for(int i = 1; i <= Math.sqrt(x); i++) {
+        //     if(i * i <= x) {
+        //         ans = i;
+        //     }
+        // }
+        // return ans;
+
+        int low = 0, high = (int) Math.sqrt(x);
+        while(low <= high) {
+            int mid = (low + high) / 2;
+            if(mid * mid == x) return mid;
+            else if(mid * mid > x) high = mid - 1;
+            else low = mid + 1;
         }
-        return ans;
+        return high;
     }
 }
