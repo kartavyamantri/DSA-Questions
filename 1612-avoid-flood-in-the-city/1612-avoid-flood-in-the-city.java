78dbf4ebec0c44c98f441088e22035f12ead2e41
Kartavya Mantri <127770922+kartavyamantri@users.noreply.github.com>
2025-10-08 00:01:02 +0530

    Time: 54 ms (97.26%), Space: 62.7 MB (26.03%) - LeetHub

--- 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.java
+++ 1612-avoid-flood-in-the-city/1612-avoid-flood-in-the-city.java
@@ -1,26 +1,61 @@
 class Solution {
-
     public int[] avoidFlood(int[] rains) {
-        int[] ans = new int[rains.length];
-        Arrays.fill(ans, 1);
-        TreeSet<Integer> st = new TreeSet<Integer>();
-        Map<Integer, Integer> mp = new HashMap<Integer, Integer>();
-        for (int i = 0; i < rains.length; ++i) {
-            if (rains[i] == 0) {
-                st.add(i);
-            } else {
-                ans[i] = -1;
-                if (mp.containsKey(rains[i])) {
-                    Integer it = st.ceiling(mp.get(rains[i]));
-                    if (it == null) {
-                        return new int[0];
-                    }
-                    ans[it] = rains[i];
-                    st.remove(it);
+        int ans[] = new int[rains.length];
+        int last[] = new int[rains.length];
+        HashMap<Integer,Integer> track = new HashMap<>();
+        PriorityQueue<Integer> pq = new PriorityQueue<>();
+
+        for(int i=rains.length-1; i>=0 ; i--){
+            int num = rains[i];
+            if(num==0){
+                continue;
+            }
+            if(track.containsKey(num)){
+                last[i] = track.get(num);
+            }
+            else{
+                last[i] = -1;
+            }
+            track.put(num,i);
+        }
+
+        HashMap<Integer,Integer> map = new HashMap<>();
+        for(int i=0;i<rains.length;i++){
+            if(rains[i]!=0){
+                int num = rains[i];
+                if(map.containsKey(num)){
+                    
+                    return new int[0];
                 }
-                mp.put(rains[i], i);
+
+                map.put(num,1);
+                ans[i] = -1;
+                if(last[i]!=-1){pq.add(last[i]);}
+            }
+            else{
+
+                if(map.size()==0 || i==rains.length-1){
+                    ans[i] = 1;
+                    continue; 
+                }
+
+                if(pq.isEmpty()){
+                    ans[i] = 1;
+                    continue;
+                }
+                int idx = pq.poll();
+                if(idx==-1){
+                    ans[i] = 1;
+                }
+                else{
+                    ans[i] = rains[idx];
+                    map.remove(rains[idx]);
+                }
+                
+               
             }
         }
+
         return ans;
     }
 }
